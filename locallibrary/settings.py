5be11861ff76b31b931df5eb3c0c0a6885cbf094
lucien_chen <lucien_chen@cmoney.com.tw>
2023-06-12 15:15:07 +0800

    setting.py -- allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = True
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://librarysystem.onrender.com']
+ALLOWED_HOSTS = ['librarysystem.onrender.com']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
